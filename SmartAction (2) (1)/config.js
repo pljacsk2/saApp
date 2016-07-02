8efd0281add3a7a799a395b83882a016fbf3449d
Paul Jackson <pauljackson@Pauls-MacBook-Pro.local>
2016-07-02 14:20:56 -0700

    Add existing file

---
+++ SmartAction (2) (1)/config.js
@@ -0,0 +1,9 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "SmartAction",
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
